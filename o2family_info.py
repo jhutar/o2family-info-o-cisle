1d4de35c3b651136d908a882ccdb5562fe8d4c18
Jan Hutar <jhutar@redhat.com>
2024-02-04 23:22:40 +0100

    feat: Dump all numbers seen in UI

--- o2family_info.py
+++ o2family_info.py
@@ -4,10 +4,18 @@ import argparse
 import logging
 import logging.handlers
 import os
+import re
 import time
 import requests
 import json
 import sys
+
+
+def dir_path(path):
+    if os.path.isdir(path):
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"Path {path} is not a valid directory")
 
 
 def setup_logger(stderr_log_lvl):
@@ -44,6 +52,40 @@ def setup_logger(stderr_log_lvl):
     return logging.getLogger("root")
 
 
+from html.parser import HTMLParser
+from html.entities import name2codepoint
+
+class MyHTMLParser(HTMLParser):
+    suspected = None
+    matches = {}
+
+    def handle_starttag(self, tag, attrs):
+        self.suspected = None
+        if tag == "a":
+            for attr in attrs:
+                if len(attr) != 2:
+                    continue
+                if attr[0] != "href":
+                    continue
+                if not attr[1].startswith("/nastaveni-tarifu-a-sluzeb/"):
+                    continue
+                if not attr[1].split("/")[2].isnumeric():
+                    continue
+                self.suspected = attr[1].split("/")[2]
+
+    def handle_endtag(self, tag):
+        self.suspected = None
+
+    def handle_data(self, data):
+        if self.suspected is None:
+            return
+        data_strip = data.strip()
+        if not data_strip.isnumeric():
+            return
+        self.matches[data_strip] = self.suspected
+        self.suspected = None
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="Získá informace o O2Family čísle z jejich samoobsluhy",
@@ -62,15 +104,9 @@ def main():
         help="Heslo pro přihlášení do O2Family samoobsluhy",
     )
     parser.add_argument(
-        "--id",
-        required=True,
-        type=int,
-        help="ID vašeho čísla v O2Family samoobsluze. Je v URL když po přihlášení na číslo klikáte pro zobrazení detailů.",
-    )
-    parser.add_argument(
         "--save-as",
-        type=str,
-        help="Soubor kam zapsat zjištěná data",
+        type=dir_path,
+        help="Adresář kam zapsat zjištěná soubory se zjištěnými daty, pojmenované '<tel_cislo>.json'",
     )
     parser.add_argument(
         "--force",
@@ -108,26 +144,36 @@ def main():
     }
 
     # Log in
-    session.post("https://moje.o2family.cz/", data=data)
+    response = session.post("https://moje.o2family.cz/", data=data)
+    response.raise_for_status()
     logger.debug(f"Cookies after login: {session.cookies}")  # Expect to see PHPSESSID
 
-    # Get info
-    response = session.get(f"https://moje.o2family.cz/api/tariff-info/{args.id}")
-    logger.info(f"Received: {response.json()}")
+    # Parse phone number IDs
+    ###print(response.text)
+    parser = MyHTMLParser()
+    parser.feed(response.text)
+    for phone_number, phone_id in parser.matches.items():
+        logger.info(f"Working on number {phone_number} id {phone_id}")
 
-    if args.save_as is not None:
-        if os.path.exists(args.save_as):
-            if args.force:
-                os.remove(args.save_as)
-                logger.debug(f"Removed {args.save_as}")
-            else:
-                logger.error(
-                    f"File {args.save_as} already exists. If you want to override it, use option '--force'"
-                )
-                return 1
-        with open(args.save_as, "w") as fp:
-            json.dump(response.json(), fp)
-            logger.info(f"Dumped data to {args.save_as}")
+        # Get info about phone number
+        response = session.get(f"https://moje.o2family.cz/api/tariff-info/{phone_id}")
+        response.raise_for_status()
+        logger.debug(f"Received: {response.json()}")
+
+        if args.save_as is not None:
+            save_as_file = os.path.join(args.save_as, f"{phone_number}.json")
+            if os.path.exists(save_as_file):
+                if args.force:
+                    os.remove(save_as_file)
+                    logger.debug(f"Removed {save_as_file}")
+                else:
+                    logger.error(
+                        f"File {save_as_file} already exists. If you want to override it, use option '--force'"
+                    )
+                    return 1
+            with open(save_as_file, "w") as fp:
+                json.dump(response.json(), fp)
+                logger.info(f"Dumped data to {save_as_file}")
 
 
 if __name__ == "__main__":
